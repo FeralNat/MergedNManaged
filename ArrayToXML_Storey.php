b07ff19fe881f80dbfc1e189288cb47535d3a3b4
FeralNat <89392458+FeralNat@users.noreply.github.com>
2021-10-19 13:51:27 -0400

    Add files via upload

---
+++ ArrayToXML_Storey.php
@@ -0,0 +1,24 @@
+<?php
+	$anArray = [
+		[
+			'name' => 'Peter Parker',
+			'email' => 'peterparker@mail.com'
+		],
+		[
+			'name' => 'Clark Kent',
+			'email' => 'clarkkent@mail.com'
+		],
+		[
+			'name' => 'Harry Potter',
+			'email' => 'harrypotter@mail.com'
+		],
+	];
+	$anArray[]= array(
+		'name' => 'Nat Storey',
+		'email' => 'natstorey@mail.com'
+		);
+	var_dump($anArray)
+	//im not sure how to convert to XML and the search results all were long code
+	$characters = xml_encode($anArray);
+	$handler = fopen("characters.XML",'x+');
+?>
